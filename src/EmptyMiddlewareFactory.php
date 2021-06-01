628e1a4d465937bf5ca7c8c43d236a2ea68e0a0a
Paweł Jakowczyk <pawel.jakowczyk@getresponse.com>
2021-06-02 00:00:30 +0200

    EmptyMiddlewareFactory added

---
+++ src/EmptyMiddlewareFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PJ\Middleware;
+
+
+final class EmptyMiddlewareFactory implements MiddlewareFactory
+{
+    public function createMiddlewares(): MiddlewareCollectionInterface
+    {
+        return new MiddlewareCollection();
+    }
+}
